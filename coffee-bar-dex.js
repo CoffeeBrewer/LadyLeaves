b3dc8199c7c676508db1eb5cc0c10400e4483c39
CoffeeBrewerNL <80169906+CoffeeBrewer@users.noreply.github.com>
2025-11-21 20:14:36 +0100

    0.45

---
+++ coffee-bar-dex.js
@@ -0,0 +1,4 @@
+const CHAIN_ID       = 1234;                         // bv. 1234
+const ROUTER_ADDRESS = "0xRouterAddressHere";        // Coffee DEX router
+const LADY_ADDRESS   = "0xLadyTokenAddressHere";     // LADY of WLADY
+const BEANS_ADDRESS  = "0xBeansTokenAddressHere";    // BEANS token
